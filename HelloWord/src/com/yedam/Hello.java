9f0e448207e6cac08a2619070cce71e02edece99
pc <pc@email.com>
2024-11-27 17:48:58 +0900

    자바 시작.

---
+++ HelloWord/src/com/yedam/Hello.java
@@ -0,0 +1,13 @@
+package com.yedam;
+//대소문자 구분( A-a서로 다름)
+public class Hello {
+	//메인 메소드.
+	public static void main(String[] args) {
+		//콘솔 Hello, Word 출력
+		System.out.println("Hello, Word");
+		
+		//Hello.java -> Hello.class
+		//컴파일(저장하면 eclipse 컴파일)
+		//HelloWord 프로젝트 -Hello
+	}
+}
